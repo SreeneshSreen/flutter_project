8b8fbed11af52b5317379e24b7491bf30b1b7440
SreeneshSreen <sreenesh.munderi@gmail.com>
2025-02-24 16:59:28 +0530

    new

---
+++ lib/pages/sign in.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  const Signin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      width: 400,
+      color: const Color.fromARGB(255, 53, 42, 11),
+    );
+  }
+}
